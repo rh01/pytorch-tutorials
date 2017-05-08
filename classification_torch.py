59500c0409e14a9f9d205fd2f484844ecb313f7b
rh01 <1048157315@qq.com>
2017-05-08 08:12:14 +0800

    add classification

--- classification_torch.py
+++ classification_torch.py
@@ -4,3 +4,16 @@ from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
 n_data = torch.ones(100, 2)
+x0 = torch.normal(2*n_data, 1)  # class0 x data (tensor), shape=(100, 2)
+y0 = torch.zeros(100)           # class0 x label (tensor), shape=(100, 1)
+x1 = torch.normal(-2*n_data, 1) # class1 x data (tensor), shape=(100, 2)
+y1 = torch.ones(100)            # class1 x label (tensor), shape=(100, 1)
+
+x = torch.cat((x0, x1), 0).type(torch.FloatTensor)
+y = torch.cat((y0, y1), ).type(torch.LongTensor)
+
+x, y = Variable(x), Variable(y)
+
+plt.scatter(x.data.numpy()[:, 0],x.data.numpy()[:, 1], c=y.data.numpy(), s=100, lw=0, cmap='RdYlGn')
+plt.show()
+
