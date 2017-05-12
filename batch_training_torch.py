ca3466187d1bb2dd2e8af35694a5ccc04e327e60
rh01 <1048157315@qq.com>
2017-05-12 15:46:36 +0800

    cnn and GAN network model.

---
+++ batch_training_torch.py
@@ -0,0 +1,24 @@
+import torch
+import torch.utils.data as Data
+
+
+BATCH_SIZE = 8
+
+x = torch.linspace(1, 10, 10)
+y = torch.linspace(10, 1, 10)
+
+torch_dataset = Data.TensorDataset(data_tensor=x, target_tensor=y)
+loader = Data.DataLoader(
+    dataset=torch_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    num_workers=2,
+)
+
+if __name__ == '__main__':
+    for epoch in range(3):
+        for step, (batch_x, batch_y) in enumerate(loader):
+            # training...
+            print('Epoch: ', epoch, '| Step: ', step, '| batch x: ', batch_x.numpy(),
+                  '| batch y: ', batch_y.numpy())
+
